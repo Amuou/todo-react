a482a86df74ad1c9a7c14f05250d778ede2708ca
Arthur <afigy@Arthurs-iMac.local>
2019-02-17 22:22:25 +0200

    Updates

---
+++ precache-manifest.1ec870002ae551d23a745f6ae1c5662c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2b7ce5e9490f010eac9",
+    "url": "/todo-react/static/css/main.c851c6bf.chunk.css"
+  },
+  {
+    "revision": "a2b7ce5e9490f010eac9",
+    "url": "/todo-react/static/js/main.a2b7ce5e.chunk.js"
+  },
+  {
+    "revision": "3a890a3f15edfdf19ca0",
+    "url": "/todo-react/static/js/runtime~main.3a890a3f.js"
+  },
+  {
+    "revision": "24e53f978bb142566f6e",
+    "url": "/todo-react/static/js/2.24e53f97.chunk.js"
+  },
+  {
+    "revision": "3e80fc684961e1d7cd5bcf3194eb5a71",
+    "url": "/todo-react/index.html"
+  }
+];
