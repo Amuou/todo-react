475cff806abd1fe7a39097994f954e0709c6bc7f
Amuou <faliyarthur@gmail.com>
2019-04-30 13:43:11 +0300

    Updates

---
+++ precache-manifest.c9359aac44fc4e15e5eb18d898e3a6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5dc2b07c2aa159c4fbb6506be760f0",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "6010eb079702cfcc2e8a",
+    "url": "/todo-react/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f32c7dd19edc72338eb6",
+    "url": "/todo-react/static/css/main.e806fb73.chunk.css"
+  },
+  {
+    "revision": "6010eb079702cfcc2e8a",
+    "url": "/todo-react/static/js/2.c329315b.chunk.js"
+  },
+  {
+    "revision": "f32c7dd19edc72338eb6",
+    "url": "/todo-react/static/js/main.239d03e6.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  }
+]);
